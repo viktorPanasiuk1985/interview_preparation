104d269504057c2dad61ae66416dd7d06c4bdd09
PanasiukViktor <vitek.panasyuk@gmail.com>
2026-01-20 05:11:54 +0100

    Lesson 1

---
+++ InterviewPreparation/src/lesson1/TwoPointers.java
@@ -0,0 +1,12 @@
+package lesson1;
+
+public class TwoPointers {
+
+    public void reverse(int[] arr) {
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = temp;
+        }
+    }
+}
