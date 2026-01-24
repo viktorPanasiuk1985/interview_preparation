6372a10408d11d60f964003263e7c6bbe81818e0
PanasiukViktor <vitek.panasyuk@gmail.com>
2026-01-24 01:37:19 +0100

    Lesson 1: HM

--- InterviewPreparation/src/lesson1/TwoPointers.java
+++ InterviewPreparation/src/lesson1/TwoPointers.java
@@ -1,6 +1,14 @@
 package lesson1;
 
+import java.util.Arrays;
+
 public class TwoPointers {
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {1,2,2,3};
+        removeDuplicate(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 
     public void reverse(int[] arr) {
         for (int i = 0; i < arr.length / 2; i++) {
@@ -9,4 +17,26 @@ public class TwoPointers {
             arr[arr.length - i - 1] = temp;
         }
     }
+
+    // 1 1 1 1 2 2 3 3 4 4 5 6 7 8
+
+    // Step 1
+    // i = 0 : 1
+    // j = 1 : 1
+
+    // 1 2 2 3
+
+    public static int removeDuplicate(int[] arr) {
+        int i = 0;
+        for (int j = 1; j < arr.length; j++) {
+            System.out.print(arr[j] + " ");
+            if (arr[i] != arr[j]) {
+                i++;
+                arr[i] = arr[j];
+                System.out.println(arr[i]);
+            }
+        }
+        return i + 1;
+    }
+
 }
